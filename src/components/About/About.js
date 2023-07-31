59c767086059d19c8d7f3a0d79fe6895b506a26e
Weiran Li <weiran@second.dev>
2023-07-31 11:32:15 -0700

    Second[Agent] cra to next.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import Github from "./Github";
@@ -52,3 +52,65 @@ function About() {
 }
 
 export default About;
+
+// UserProfileProviderClientComponent.js
+import React, { useState, useEffect } from 'react';
+
+const UserProfileProviderClientComponent = ({ userId, children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/users/${userId}`)
+      .then(response => response.json())
+      .then(data => setUser(data));
+  }, [userId]);
+
+  return (
+    <UserContext.Provider value={user}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProfileProviderClientComponent;
+
+// UserProfileStatusClientComponent.js
+import React, { useState } from 'react';
+
+const UserProfileStatusClientComponent = () => {
+  const [status, setStatus] = useState('active');
+
+  const changeStatus = () => {
+    setStatus(status === 'active' ? 'inactive' : 'active');
+  };
+
+  return (
+    <button onClick={changeStatus}>
+      Set status to {status === 'active' ? 'inactive' : 'active'}
+    </button>
+  );
+};
+
+export default UserProfileStatusClientComponent;
+
+// UserProfile.js
+import React from 'react';
+import UserProfileProviderClientComponent from './UserProfileProviderClientComponent';
+import UserProfileStatusClientComponent from './UserProfileStatusClientComponent';
+
+const UserProfile = ({ userId }) => {
+  return (
+    <UserProfileProviderClientComponent userId={userId}>
+      <UserContext.Consumer>
+        {user => (
+          <div>
+            <h1>{user ? user.name : 'Loading...'}</h1>
+            <UserProfileStatusClientComponent />
+          </div>
+        )}
+      </UserContext.Consumer>
+    </UserProfileProviderClientComponent>
+  );
+};
+
+export default UserProfile;
---
+++ src/components/Home/UserProfile.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import UserProfileProviderClientComponent from './UserProfileProviderClientComponent';
+import UserProfileStatusClientComponent from './UserProfileStatusClientComponent';
+
+const UserProfile = ({ userId }) => {
+  return (
+    <UserProfileProviderClientComponent userId={userId}>
+      <UserContext.Consumer>
+        {user => (
+          <div>
+            <h1>{user ? user.name : 'Loading...'}</h1>
+            <UserProfileStatusClientComponent />
+          </div>
+        )}
+      </UserContext.Consumer>
+    </UserProfileProviderClientComponent>
+  );
+};
+
+export default UserProfile;
+```
