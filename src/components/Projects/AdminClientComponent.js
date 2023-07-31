71c3b9434060f015d3dbcf95ae3c4a6adc0dbce4
Weiran Li <weiran@second.dev>
2023-07-31 12:23:00 -0700

    Second[Agent] cra to next.js

---
+++ src/components/Projects/AdminClientComponent.js
@@ -0,0 +1,26 @@
+"use client";
+import React, { useState } from 'react';
+
+const AdminClientComponent = ({ user }) => {
+  const [isExtendedView, setIsExtendedView] = useState(false);
+
+  const toggleView = () => {
+    setIsExtendedView(!isExtendedView);
+  };
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>{user.email}</p>
+      {isExtendedView && <p>{user.extendedProfileInfo}</p>}
+      <button onClick={toggleView}>Toggle View</button>
+    </div>
+  );
+};
+
+export default AdminClientComponent;
+```
