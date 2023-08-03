6958719022da6cf19e1e948ce475cc1774715d57
Weiran Li <weiran@second.dev>
2023-08-03 10:53:14 -0700

    Second[Agent] cra to next.js

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,31 +1,28 @@
+'use client'
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import Particle from "../../src/components/Particle";
+import pdf from "public/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
-
   useEffect(() => {
     setWidth(window.innerWidth);
   }, []);
-
-  return (
-    <div>
+  return <div>
       <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
+        <Row style={{
+        justifyContent: "center",
+        position: "relative"
+      }}>
+          <Button variant="primary" href={pdf} target="_blank" style={{
+          maxWidth: "250px"
+        }}>
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
@@ -37,20 +34,18 @@ function ResumeNew() {
           </Document>
         </Row>
 
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
+        <Row style={{
+        justifyContent: "center",
+        position: "relative"
+      }}>
+          <Button variant="primary" href={pdf} target="_blank" style={{
+          maxWidth: "250px"
+        }}>
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
         </Row>
       </Container>
-    </div>
-  );
+    </div>;
 }
-
 export default ResumeNew;
