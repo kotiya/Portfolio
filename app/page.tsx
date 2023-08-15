f187621ff1c7c58f9fcb536b10f6012310d9a163
Weiran Li <weiran@second.dev>
2023-08-15 14:39:46 -0700

    Second[Agent] cra to next.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
-import Particle from "../Particle";
-import Home2 from "./Home2";
-import Type from "./Type";
+import Particle from "../src/components/Particle";
+import Home2 from "../src/components/Home/Home2";
+import Type from "../src/components/Home/Type";
+import Image from 'next/image';
 
 function Home() {
   return (
@@ -31,11 +31,12 @@ function Home() {
             </Col>
 
             <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
+              <Image
+                src="/Assets/home-main.svg"
                 alt="home pic"
                 className="img-fluid"
-                style={{ maxHeight: "450px" }}
+                width={500}
+                height={450}
               />
             </Col>
           </Row>
--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
-import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import ProjectCard from "../../src/components/Projects/ProjectCards";
+import Particle from "../../src/components/Particle";
+import leaf from "public/Assets/Projects/leaf.png";
+import emotion from "public/Assets/Projects/emotion.png";
+import editor from "public/Assets/Projects/codeEditor.png";
+import chatify from "public/Assets/Projects/chatify.png";
+import suicide from "public/Assets/Projects/suicide.png";
+import bitsOfCode from "public/Assets/Projects/blog.png";
 
 function Projects() {
   return (
