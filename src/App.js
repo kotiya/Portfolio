5dc5e3add35a1372b6a7484c0f5f69b17552186f
root <root@ip-10-0-0-29.us-west-1.compute.internal>
2023-08-17 21:45:40 +0000

    Second[Agent] cra to next.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
+import { useRouter } from "next/router";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
@@ -19,6 +14,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [load, upadateLoad] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -29,21 +25,21 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <div>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
         <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
-        </Routes>
+        <div>
+          {router.pathname === "/" && <Home />}
+          {router.pathname === "/project" && <Projects />}
+          {router.pathname === "/about" && <About />}
+          {router.pathname === "/resume" && <Resume />}
+          {router.pathname !== "/" && router.pathname !== "/project" && router.pathname !== "/about" && router.pathname !== "/resume" && <Navigate to="/" />}
+        </div>
         <Footer />
       </div>
-    </Router>
+    </div>
   );
 }
 
