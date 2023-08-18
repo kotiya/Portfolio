7242100b9bf288dba2414e46c288b469594a6503
root <root@ip-10-0-0-6.us-west-1.compute.internal>
2023-08-18 17:12:00 +0000

    Second[Agent] cra to next.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,16 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
+import { useRouter } from "next/router";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Link from 'next/link';
 
 function App() {
   const [load, upadateLoad] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -29,7 +26,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <div>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
@@ -39,11 +36,11 @@ function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
+          <Route path="*" element={<Link href="/"><a>{router.push("/")}</a></Link>} />
         </Routes>
         <Footer />
       </div>
-    </Router>
+    </div>
   );
 }
 
