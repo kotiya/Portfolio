d1ff28ceae17bd2bd93f0b7ed7f1fca584c5916a
Weiran Li <weiran@second.dev>
2023-08-21 15:52:09 -0700

    Second[Agent] cra to next.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
+import { Link } from "next/link";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
@@ -29,21 +24,34 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
         <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
-        </Routes>
+        <Link href="/">
+          <a>
+            <Home />
+          </a>
+        </Link>
+        <Link href="/project">
+          <a>
+            <Projects />
+          </a>
+        </Link>
+        <Link href="/about">
+          <a>
+            <About />
+          </a>
+        </Link>
+        <Link href="/resume">
+          <a>
+            <Resume />
+          </a>
+        </Link>
         <Footer />
       </div>
-    </Router>
+    </>
   );
 }
 
