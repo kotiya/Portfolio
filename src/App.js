6590bf5a443daaccb3f972beae53a2e8ab8c2f91
root <root@ip-10-0-0-219.us-west-1.compute.internal>
2023-08-22 03:04:29 +0000

    Second[Agent] cra to next.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
+import { useRouter } from 'next/router'; // Import the useRouter hook from Next.js
+import Link from 'next/link'; // Import the Link component from 'next/link'
+
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
@@ -19,6 +16,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [load, upadateLoad] = useState(true);
+  const router = useRouter(); // Add the useRouter hook
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -29,7 +27,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <div>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
@@ -39,11 +37,11 @@ function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
       </div>
-    </Router>
+    </div>
   );
 }
 
